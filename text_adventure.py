05d174d4d3607a3c29a55f97029b9a550be24468
XanderCatR999 <81835052+XanderCatR999@users.noreply.github.com>
2025-05-06 19:11:13 -0700

    added XORKOID python branch

---
+++ text_adventure.py
@@ -0,0 +1,45 @@
+# text stuff
+
+choice1 = input("""You wake up in a dimly lit dungeon what do you do? you see a door to your left and a sewer grate to you right and a axe in front of you. do you do any of the aforementioned options?""")
+
+if choice1 == 'door':
+    print("""as you arrive at the door you die via 'exploding colon while near door syndrome'.""")
+    exit()
+elif choice1 == 'sewer':
+    print("""as you attempt to lift the sewer grate you lift and twist and promptly die from a brain hemorrhage.""")
+    exit()
+elif choice1 == 'axe':
+    print("""as you approach the axe you notice someone hiding in the shadows with a sword so you swiftly grab the axe and injure the person. as you injure the person you wake up in your bed at your house""")
+elif choice1 == 'secret':
+    print("""You realize that you're in a dream and wake up immediately.""")
+else:
+    print("""INVALID CHOICE TRY 'door', 'sewer', or 'axe'.""")
+    exit()
+choice2 = input("""as you wake up, you realize that the alarm clock, by your bed, has fallen over and smashed in by what appears to be your fathers 55,603 comic books. you see a lighter and a can of gasoline. do you light the 55,603 comics on fire? Yes or No :   """)
+
+if choice2 == 'yes':
+    print("""as you attempt to light your fathers 55, you get knocked out by your father and wake up in the bathroom with your left kidney gone and you feel like your liver is also gone. then you die of a brain aneurysm. GAME OVER """)
+    exit()
+elif choice2 == 'no':
+    print("""as you noclip through the comics you see that your father can not see you so you goto school and get shot down by the USAF for not having the proper permits for noclipping. GAME OVER """)
+    exit()
+elif choice2 == 'wake up':
+    print("""you wake up again and see that you are up at 3:00 AM and then get up. THE END...                                                                                                                                                                                                                                                  GOOD ENDING""")
+else:
+    print("""INVALID CHOICE : TRY 'yes' or 'no'.""")
+    exit()
+
+
+choice3 = input("""as you get up you slip fall and pass out from blunt force trauma. THREE DAYS LATER... you wake up from a coma and hear zombie noises do you kill the person outside your room? :   """)
+
+if choice3 == 'yes':
+    print("""you kill the 'thing' outside your room and promptly get arrested for 1st degree murder. GAME OVER""")
+elif choice3 ==  'no':
+    print("""CONGRATS YOU SURVIVED 'THE INCIDENT'..... GREAT ENDING""")
+
+
+
+
+
+
+# Xander's code
